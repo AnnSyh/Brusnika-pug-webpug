9c0fc440a83813192040b5dd4c0f6bac7e7b3aa6
AnnSyh <ann.syharevskaya@gmail.com>
2022-07-28 17:38:01 +0300

    fix: menu card

---
+++ js/plugins/scrollPage.js
@@ -0,0 +1,14 @@
+console.log('scroll');
+
+// $(document).on("scroll", window, function () {
+//     if ($(window).scrollTop() > 200) {
+//         $(".scroll").show();
+//     }
+//     else {
+//         $(".scroll").hide();
+//     }
+// });
+
+window.addEventListener('scroll', function() {
+    console.log('scroll');
+  });
