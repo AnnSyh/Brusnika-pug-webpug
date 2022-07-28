ced904f038245b669a1b8c16a2b1169051ac4cdf
AnnSyh <ann.syharevskaya@gmail.com>
2022-07-28 23:48:27 +0300

    some edits

--- js/plugins/swipe.js
+++ js/plugins/swipe.js
@@ -1,9 +1,9 @@
-import PhotoSwipeLightbox from '/js/plugins/photoswipe-lightbox.esm.js';
+import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
 
 const lightbox = new PhotoSwipeLightbox({
     gallery: '#my-gallery',
     children: 'a',
-    pswpModule: () => import('/js/plugins/photoswipe.esm.js'),
+    pswpModule: () => import('./photoswipe.esm.js'),
 });
 
 lightbox.init();
